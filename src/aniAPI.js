e0e73497ea7c90f7cfa82031e1b264355e6dabe5
PatoGM <patriciogm@live.com>
2021-11-06 17:04:12 -0500

    Confusion

--- src/aniAPI.js
+++ src/aniAPI.js
@@ -1,11 +1,11 @@
-import fetch from 'node-fetch';
+const {fetch} = require("../main");
 
 let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MyIsIm5iZiI6MTYzNjIzMzY4OSwiZXhwIjoxNjM4ODI1Njg5LCJpYXQiOjE2MzYyMzM2ODl9.JMW-W9UktIgigppNsURGRnWNQEfRhgsrqq_iQtZem4s";
 
 let permission =   fetch('https://api.aniapi.com/v1/user_story', {
     method: 'GET',
     headers: {
-      'Authorization': 'Bearer <YOUR_JWT>',
+      'Authorization': `Bearer ${token}`,
       'Content-Type': 'application/json',
       'Accept': 'application/json'
     }
