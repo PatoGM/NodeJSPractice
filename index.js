80e49aa90c14ad711b25a8cc52582f0d3ae6e52a
PatoGM <patriciogm@live.com>
2021-11-06 21:25:34 -0500

    Close

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Set options as a parameter, environment variable, or rc file.
 // eslint-disable-next-line no-global-assign
-let options = "";
 require = require("esm")(module/*, options*/);
 module.exports = require("./main.js");
--- src/aniAPI.js
+++ src/aniAPI.js
@@ -1,3 +1,5 @@
+import {fetch} from "node-fetch";
+ 
 let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MyIsIm5iZiI6MTYzNjIzMzY4OSwiZXhwIjoxNjM4ODI1Njg5LCJpYXQiOjE2MzYyMzM2ODl9.JMW-W9UktIgigppNsURGRnWNQEfRhgsrqq_iQtZem4s";
 
 let permission =   fetch('https://api.aniapi.com/v1/user_story', {
